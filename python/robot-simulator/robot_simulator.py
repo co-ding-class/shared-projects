163989fec517b2b05cdd781210ee22ff1a00d67b
Coding Class <jspjohnston@gmail.com>
2018-10-12 19:46:12 -0400

    just cstarting sims-you-laser

---
+++ python/robot-simulator/robot_simulator.py
@@ -0,0 +1,18 @@
+# Globals for the bearings
+# Change the values as you see fit
+EAST = None
+NORTH = None
+WEST = None
+SOUTH = None
+
+
+class Robot(object):
+
+	def __init__(self, bearing=NORTH, x=0, y=0):
+		coordinates = (x, y)
+
+	def advance(self):
+		pass
+
+	def simulate(self, direction):
+		pass
