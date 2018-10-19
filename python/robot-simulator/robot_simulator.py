98a56759da1ad89bf46228b258f34972e5374245
Coding Class <jspjohnston@gmail.com>
2018-10-19 19:32:07 -0400

    Started on simulate function today.

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -4,15 +4,31 @@ EAST = None
 NORTH = None
 WEST = None
 SOUTH = None
+"""
 
-
+"""
 class Robot(object):
 
 	def __init__(self, bearing=NORTH, x=0, y=0):
-		coordinates = (x, y)
+		self.coordinates = (x, y)
 		self.bearing = bearing
+		
+	def simulate(self, motion):
+		for apricot in motion:
+			if apricot.upper() == "L":
+				self.turn_left()
+				
+			elif apricot.upper() == "R": 
+				self.turn_right()
+				
+			elif apricot.upper() == "A":
+				self.advance()
+				
 	def advance(self):
 		pass
 
-	def simulate(self, direction):
+	def turn_left(self):
+		pass
+		
+	def turn_right(self):
 		pass
