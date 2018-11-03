8ea37a8601f0aac5452223bc7b2cd5cf8a4acd7b
coding class <jspjohnston@gmail.com>
2018-11-02 17:37:59 -0700

    Our work here is not finished.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,10 @@
 def on_square(integer_number):
+	if (integer_number) < 0:
+		raise ValueError
 	return 2 ** (integer_number - 1)
-
+	
 def total_after(integer_number):
-    return (2 **(integer_number + 1)) - 1
+	if (integer_number) < 0:
+		raise ValueError
+	return 2 ** integer_number - 1
 
