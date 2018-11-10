20257088bbf74f1494c086febb262f2af2f22f76
Coding Class <jspjohnston@gmail.com>
2018-11-09 19:32:49 -0500

    Yay, we finished! Lots of grains on the chessboard now!<=

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,10 +1,11 @@
 def on_square(integer_number):
-	if (integer_number) < 0:
-		raise ValueError
-	return 2 ** (integer_number - 1)
+	if (integer_number) <= 0 or integer_number > 64:
+		raise ValueError("that's not a square!")
+	grain_number = 2 ** (integer_number - 1)
+	return grain_number
 	
 def total_after(integer_number):
-	if (integer_number) < 0:
-		raise ValueError
+	if (integer_number) <= 0 or integer_number > 64:
+		raise ValueError("that's not a square!")
 	return 2 ** integer_number - 1
 
