5ca9e4bc293435d6435b9e177835e1fcfc6bb880
Coding Class <jspjohnston@gmail.com>
2018-11-16 19:37:49 -0500

    Tested on new class: Pickle to relate to linked lists.

--- python/linked-list/linked_list.py
+++ python/linked-list/linked_list.py
@@ -1,7 +1,8 @@
 class Node(object):
     def __init__(self, value, succeeding=None, previous=None):
-        pass
-
+       self.value = value
+	   self.next = succeeding
+	   self.back = previous
 
 class LinkedList(object):
     def __init__(self):
