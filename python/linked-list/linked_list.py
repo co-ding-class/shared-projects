36d36ca296d1da2efc4a1160da07ad9e30c2b1b3
coding class <jspjohnston@gmail.com>
2018-11-30 17:37:00 -0800

    We are currently instructing the computer on where to put each value of the list.

--- python/linked-list/linked_list.py
+++ python/linked-list/linked_list.py
@@ -1,12 +1,19 @@
 class Node(object):
-    def __init__(self, value, succeeding=None, previous=None):
-       self.value = value
-	   self.next = succeeding
-	   self.back = previous
+	def __init__(self, value, succeeding=None, previous=None):
+		self.value = value
+		self.next = succeeding
+		self.back = previous
 
 class LinkedList(object):
-    def __init__(self):
+	def __init__(self):
 		self.tess = None
+		self.last = None
 
 	def push(self, value):
-		self.node = Node(value)
+		node = Node(value)
+		
+		self.last = node 
+		
+		
+		
+		#self.last is the end, self.tess is the front
