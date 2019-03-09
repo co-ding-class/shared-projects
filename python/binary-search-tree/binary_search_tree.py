dc26278730a1ac2d83dbc0af84a66f27b83e1783
My name <example@example.com>
2019-03-08 19:34:23 -0500

    worked hard and used 'super sheet'

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,32 +1,41 @@
 class TreeNode(object):
-    def __init__(self, data, left = None, right = None):
-        self.data = data
-        self.left = left
-        self.right = right
+	def __init__(self, data, left = None, right = None):
+		self.data = data
+		self.left = left
+		self.right = right
 
-    def __str__(self):
-        fmt = 'TreeNode(data={}, left={}, right={})'
-        return fmt.format(self.data, self.left, self.right)
+	def __str__(self):
+		fmt = 'TreeNode(data={}, left={}, right={})'
+		return fmt.format(self.data, self.left, self.right)
 
 
 class BinarySearchTree(object):
-    def __init__(self, tree_data): # ['4']
-        self.root = None
-        for leaf in (tree_data):
-            pass
+	def __init__(self, tree_data):
+		self.root = None
 
-    def data(self):
-        return 
+		for leaf in (tree_data):
+	
+	
+	def add(self):
+		inserted = False
+		while not inserted:
+			if cur_node.left:
+				
+			else:
 
-    def sorted_data(self):
-        pass
+		el
+				
+	def data(self):
+		return 
+
+	def sorted_data(self):
+		pass
 		
-	def inorder(self, root, left, right):		
-	    if root == None: return
+	def inorder(self, root, left, right):
+		if root == None: return
+		
 		def recursive_function(root):
-		    if root.left == None and root.right == None: return
+			if root.left == None and root.right == None: return
 			recursive_function(root.left)
 			# TODO: process the data
 			recursive_function(root.right)
-		
-		
